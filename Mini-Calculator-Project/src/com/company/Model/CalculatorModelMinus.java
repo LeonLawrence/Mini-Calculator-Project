dd54080b7dc6dce7813328f7e1a4471e0efc2b51
LeonLawrence <39957915+LeonLawrence@users.noreply.github.com>
2020-08-03 22:18:24 +0100

    MVC set up, and code structure built

---
+++ Mini-Calculator-Project/src/com/company/Model/CalculatorModelMinus.java
@@ -0,0 +1,13 @@
+package com.company.Model;
+
+public class CalculatorModelMinus {
+    private int calculationValue;
+
+    public void minusTwoNumbers(int firstNumber, int secondNumber) {
+        calculationValue = firstNumber + secondNumber;
+    }
+
+    public int getCalculation() {
+        return calculationValue;
+    }
+}
