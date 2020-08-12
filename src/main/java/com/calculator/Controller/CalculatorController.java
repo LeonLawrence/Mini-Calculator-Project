436706d590e01e5f578237a9af10a709e4a9cf38
LeonLawrence <leonlawrence@outlook.com>
2020-08-12 19:57:41 +0100

    Refactored code for GUI and set GUI keys as private in gobal scope

--- src/main/java/com/calculator/Controller/CalculatorController.java
+++ src/main/java/com/calculator/Controller/CalculatorController.java
@@ -2,29 +2,34 @@ package com.calculator.Controller;
 
 import com.calculator.Model.CalculatorModel;
 import com.calculator.View.CalculatorView;
+import com.calculator.View.CalculatorViewGUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CalculatorController {
     private CalculatorModel theModel;
-    private CalculatorView theView;
+    private CalculatorViewGUI theView;
 
-    public CalculatorController(CalculatorModel theModel, CalculatorView theView) {
+
+    public CalculatorController(CalculatorModel theModel, CalculatorViewGUI theView) {
         this.theModel = theModel;
         this.theView = theView;
     }
-
-    public void displayResult() {
-        theView.displayResult(theModel.getCalculation());
-    }
-
-    public void add(int firstNumber, int secondNumber) {
-        theModel.addTwoNumber(firstNumber, secondNumber);
-    }
-
-    public void minus(int firstNumber, int secondNumber) {
-        theModel.minusTwoNumber(firstNumber, secondNumber);
-    }
-
-    public void multiply(int firstNumber, int secondNumber) {
-        theModel.multiplyTwoNumber(firstNumber, secondNumber);
-    }
 }
+
+//    public void displayResult() {
+//        theView.displayResult(theModel.getCalculation());
+//    }
+
+//    public void add(int firstNumber, int secondNumber) {
+//        theModel.addTwoNumber(firstNumber, secondNumber);
+//    }
+//
+//    public void minus(int firstNumber, int secondNumber) {
+//        theModel.minusTwoNumber(firstNumber, secondNumber);
+//    }
+//
+//    public void multiply(int firstNumber, int secondNumber) {
+//        theModel.multiplyTwoNumber(firstNumber, secondNumber);
+//    }
--- src/main/java/com/calculator/View/CalculatorViewGUI.java
+++ src/main/java/com/calculator/View/CalculatorViewGUI.java
@@ -2,59 +2,77 @@ package com.calculator.View;
 
 import javax.swing.*;
 
-public class CalculatorViewGUI {
+public class CalculatorViewGUI extends JFrame {
+    private JLabel firstNumLabel;
+    private JTextField firstNumber;
+    private JLabel secondNumLabel;
+    private JTextField secondNumber;
+    private JButton buttonAdd;
+    private JButton buttonMinus;
+    private JButton buttonMultiply;
+    private JLabel calculationAdd;
+    private JLabel calculationMinus;
+    private JLabel calculationMultiply;
 
-    public static void body() {
+    public void body() {
         JPanel panel = new JPanel();
         JFrame frame = new JFrame();
-        frame.setSize(350,200);
+        frame.setSize(350, 200);
         frame.add(panel);
 
-        JLabel titleLabel = new JLabel("**Please Enter Two Numbers**");
-        titleLabel.setBounds(10, 20, 80, 25);
-//        panel.add(titleLabel);
-
-        JLabel firstNumLabel = new JLabel("First Num");
+        firstNumLabel = new JLabel("First Num");
         firstNumLabel.setBounds(10, 20, 80, 25);
         panel.add(firstNumLabel);
 
-        JTextField firstNumText = new JTextField(20);
-        firstNumText.setBounds(100,20,165,25);
-        panel.add(firstNumText);
+        firstNumber = new JTextField(20);
+        firstNumber.setBounds(100, 20, 165, 25);
+        panel.add(firstNumber);
 
-        JLabel secondNumLabel = new JLabel("Second Num");
+        secondNumLabel = new JLabel("Second Num");
         secondNumLabel.setBounds(10, 50, 80, 25);
         panel.add(secondNumLabel);
 
-        JTextField secondNumText = new JTextField(20);
-        secondNumText.setBounds(100,50,165,25);
-        panel.add(secondNumText);
+        secondNumber = new JTextField(20);
+        secondNumber.setBounds(100, 50, 165, 25);
+        panel.add(secondNumber);
 
-        JButton buttonAdd = new JButton("ADD");
+        buttonAdd = new JButton("ADD");
         buttonAdd.setBounds(10, 80, 80, 25);
         panel.add(buttonAdd);
 
-        JButton buttonMinus = new JButton("MINUS");
+        buttonMinus = new JButton("MINUS");
         buttonAdd.setBounds(20, 80, 80, 25);
         panel.add(buttonMinus);
 
-        JButton buttonMultiply = new JButton("MULTIPLY");
+        buttonMultiply = new JButton("MULTIPLY");
         buttonAdd.setBounds(30, 80, 80, 25);
         panel.add(buttonMultiply);
 
-        JLabel calculationAdd = new JLabel("*");
+        calculationAdd = new JLabel("*");
         calculationAdd.setBounds(10, 110, 300, 25);
         panel.add(calculationAdd);
 
-        JLabel calculationMinus = new JLabel("*");
+        calculationMinus = new JLabel("*");
         calculationMinus.setBounds(20, 110, 300, 25);
         panel.add(calculationMinus);
 
-        JLabel calculationMultiply = new JLabel("*");
+        calculationMultiply = new JLabel("*");
         calculationMultiply.setBounds(30, 110, 300, 25);
         panel.add(calculationMultiply);
 
         frame.setVisible(true);
+
+    }
+
+    public int getFirstNumber() {
+
+        return Integer.parseInt(firstNumber.getText());
+
+    }
+
+    public int getSecondNumber() {
+
+        return Integer.parseInt(secondNumber.getText());
 
     }
 }
